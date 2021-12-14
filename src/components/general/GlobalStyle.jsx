0553402a981097109b7f1a6dc5800b939cdab48c
madeleen <maria.roestorff@gmail.com>
2021-12-14 16:10:50 +0200

    BUD-13 initial commit plus font size adjustment

--- src/components/general/GlobalStyle.jsx
+++ src/components/general/GlobalStyle.jsx
@@ -12,7 +12,7 @@ body {
 * {
   box-sizing: border-box;
   outline: none;
-  font-size: 16px;
+  font-size: 18px;
   font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
     Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
 }
